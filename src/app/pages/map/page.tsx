5c860140c84243c26bc21580428fac21d3475f69
BrettPost <bwpost@mtu.edu>
2023-10-27 16:45:26 -0700

    Mapbox added to Map page

--- src/app/pages/map/page.tsx
+++ src/app/pages/map/page.tsx
@@ -1,15 +1,15 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
+import MapBox from '../../Components/Maps/MapBox'
 
 export default function Map() {
   return (
     <div>
-        <h1 className="text-3xl font-extrabold uppercase">Map of Things</h1>
-
-        <p>
-            I will build a cool page for this some day.
-        </p>
+        <div className='flex'>
+          <MapBox />
+        </div>
     </div>
   )
 }
